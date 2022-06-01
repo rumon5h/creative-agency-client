b101b5d94a76d131c6afad1e2c937d245cb8b999
rumon5 <mdrumonkhan48519@gmail.com>
2022-06-01 18:29:01 +0600

    Navbar done

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/Group 1367.png';
 
 const Navbar = () => {
+
     return (
-        <div>
-            
+        <div className='flex justify-between mt-[30px] px-7 items-center'>
+            <div>
+                <Link  to='/'>
+                    <img className='w-36 h-9' src={logo} alt="" />
+                     </Link>
+            </div>
+            <div>
+                <Link className='ml-3' to='/home'>Home</Link>
+                <Link className='ml-3' to='/portfolio'>Our Portfolio</Link>
+                <Link className='ml-3' to='/team'>Our Team</Link>
+                <Link className='ml-3' to='/contact'>Contact Us</Link>
+                <Link className='ml-3' to='/login'> <button class="btn px-9">LogIn</button></Link>
+            </div>
         </div>
     );
 };
